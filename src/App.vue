9778351c45622ba59b4e0c4d2322f68439f0edf6
hehe <1263745916@qq.com>
2023-07-31 14:56:32 +0800

    '初始化仓库'

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@ import HelloWorld from './components/HelloWorld.vue'
     <a href="https://vitejs.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
     </a>
-    <a href="https://vuejs.org/" target="_blank">
+    <a href="https://vuejs.org/" target="_blank">55555
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
